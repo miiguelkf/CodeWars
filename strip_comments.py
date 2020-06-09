c6db2f7583b85803030867e1e8e01a871695a8f2
unknown <miguelktnf@gmail.com>
2020-06-09 18:35:01 -0300

    Strip Comments

--- strip_comments.py
+++ strip_comments.py
@@ -1,4 +1,5 @@
 '''
+https://www.codewars.com/kata/51c8e37cee245da6b40000bd
 Complete the solution so that it strips all text that follows any of a set of comment markers passed in. Any whitespace at the end of the line should also be stripped out.
 
 Example:
@@ -19,17 +20,17 @@ result = solution("apples, pears # and bananas\ngrapes\nbananas !apples", ["#", 
 # result should == "apples, pears\ngrapes\nbananas"
 '''
 
-def StripComments(s,chars):
+def StripComments(s,markers):
 
     final = []
     for line in s.split('\n'):
 
-        for i , char in enumerate(chars):
+        for i , char in enumerate(markers):
             if char in line:
                 char_used = True
                 line = line.split(char)[0].strip()
 
-            if i == len(chars)-1:
+            if i == len(markers)-1:
                 final.append(line)
 
     if not final:
